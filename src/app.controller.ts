75778621a4b6fd7a8f8e271541d2ce46da61ccf1
Petro <balkans@i.ua>
2019-10-15 23:43:26 +0300

    Added jwt auth strategy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {
+    constructor(private readonly appService: AppService,
+                private readonly authService: AuthService) {
     }
 
     @Get()
@@ -15,6 +17,6 @@ export class AppController {
     @UseGuards(AuthGuard('local'))
     @Post('auth/login')
     public async login(@Request() req) {
-        return req.user;
+        return this.authService.login(req.user);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService) {
+    constructor(private readonly usersService: UsersService,
+                private readonly jwtService: JwtService) {
     }
 
     public async validateUser(username: string, pass: string): Promise<any> {
@@ -15,6 +17,13 @@ export class AuthService {
         }
 
         return null;
+    }
+
+    public async login(user: any) {
+        const payload = { username: user.username, sub: user.userId };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
     }
 
 }
