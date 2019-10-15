b34da915c154ee4b65b3111488e87263419ca198
Petro <balkans@i.ua>
2019-10-15 23:35:16 +0300

    Added local auth strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TodoModule,
     MongooseModule.forRoot('mongodb://balkanss:petya777@ds125273.mlab.com:25273/web-chat'),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
